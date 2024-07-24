f8891dad9e23e404a29ca114a293e32563d8cf4e
LaPoloche <buchon.paul@gmail.com>
2024-07-24 19:21:04 +0200

    Ajout Cors

--- api/index.ts
+++ api/index.ts
@@ -9,7 +9,21 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Configure CORS
-app.use(cors());
+//app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 const agent = new https.Agent({
   rejectUnauthorized: false,
